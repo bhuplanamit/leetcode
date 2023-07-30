8f84a910e03266da7751af2d025625c11563daf1
amit choudhary <amitchoudhary5759@gmail.com>
2023-07-30 22:14:05 +0530

    my first commit

---
+++ lc-sol/src/main/java/com/bhuplanamit/leetcode/SecondLargestNumber.java
@@ -0,0 +1,15 @@
+package com.bhuplanamit.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class SecondLargestNumber {
+
+    public static void main(String[] args) {
+        Integer arr[] = {9,10,11,12,56,13,1413};
+        ArrayList arr2 = new ArrayList();
+        arr2 = (ArrayList) Arrays.stream(arr).sorted().collect(Collectors.toList());
+        System.out.println(arr2.get(arr.length-2));
+    }
+}
